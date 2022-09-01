a0558d7da4503b73c5ce840e81bb1b9665fc048a
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2022-09-01 12:51:56 -0300

    Create Procfile to heroku deploy

--- bot.py
+++ bot.py
@@ -2,20 +2,22 @@ import discord
 
 intents = discord.Intents.default()
 intents.message_content = True
-token = ''
+token = 'MTAxMjUyNzUzNTk0ODU3MDc1NA.GyfGA3.guPW0Z8sZaY3D0upZ4UBWDYUwFamSQdyqVP2e0'
 
 client = discord.Client(intents=intents)
 
 
 @client.event
 async def on_ready():
-    global guilds, mutados, deitados
+    global guilds, mutados, deitados, alone
     print(f'We have logged in as {client.user}')
     guilds = discord.utils.get(client.guilds)
     mutados = discord.utils.get(guilds.channels, name='Mutados')
     deitados = discord.utils.get(guilds.channels, name='Trabalhando deitado')
+    alone = discord.utils.get(guilds.channels, name='SemAmigos.com.br')
     for channel in guilds.channels:
-        if channel.name != 'general' or channel.name != 'Mutados':
+        if channel.name != 'general' and channel.name != 'Mutados':
+            print(channel)
             for user in channel.members:
                 if user != client.user:
                     voice = user.voice
@@ -39,7 +41,7 @@ def check_channels():
 @client.event
 async def on_voice_state_update(member, before, after):
     if after.self_deaf or after.self_mute:
-        if member.voice.channel.name != 'Mutados':
+        if member.voice.channel.name != 'Mutados' and not member.voice.self_stream:
             await member.move_to(mutados)
     elif not after.self_deaf and not after.self_mute:
         if member.voice.channel.name == 'Mutados':
