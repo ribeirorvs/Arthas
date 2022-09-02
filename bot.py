40821afccc2bc84724f5692b2447a2af89012804
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2022-09-01 21:47:08 -0300

    Include functionalite to move members online alone

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ async def on_ready():
     deitados = discord.utils.get(guilds.channels, name='Trabalhando deitado')
     alone = discord.utils.get(guilds.channels, name='SemAmigos.com.br')
     for channel in guilds.channels:
-        if channel.name != 'general' and channel.name != 'Mutados':
+        if channel.name != 'general' and channel != mutados:
             print(channel)
             for user in channel.members:
                 if user != client.user:
@@ -29,15 +29,27 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    if message.content == '1':
+        await check_alone()
 
 def check_channels():
     for channel in guilds.channels:
-            if channel.name == 'general' or channel.name == 'Mutados':
+            if channel.name == 'general' or channel == mutados or channel == alone:
                 continue
             else:
                 if len(channel.members) > 0:
                     return channel
     return deitados
+
+def check_alone():
+    for channel in guilds.channels:
+        if channel.name == 'general' or channel == mutados or channel == alone:
+            continue
+        elif len(alone.members) > 0:
+            print(len(alone.memnbers))
+            if len(channel.members) == 1:
+                return True
+    return False
 
 @client.event
 async def on_voice_state_update(member, before, after):
@@ -45,7 +57,10 @@ async def on_voice_state_update(member, before, after):
         if member.voice.channel.name != 'Mutados' and not member.voice.self_stream:
             await member.move_to(mutados)
     elif not after.self_deaf and not after.self_mute:
-        if member.voice.channel.name == 'Mutados':
+        if member.voice.channel == mutados:
             await member.move_to(check_channels())
+        elif check_alone():
+            print('foi aqui')
+            await member.move_to(alone)
 
 client.run(token)
